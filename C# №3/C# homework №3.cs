61be5b98f61a9b6a1dd7b11d6ebe78d33a8d6651
Scelth <120787169+Scelth@users.noreply.github.com>
2023-01-01 19:13:32 +0400

    Add files via upload

---
+++ C# №3/C# homework №3.cs
@@ -0,0 +1,64 @@
+﻿#region Класс дробь
+    Fraction f1 = new(1, 2); // 1/2
+    Fraction f2 = new(2, 3); // 2/3
+
+    Fraction f3 = f1 + f2; // (1/2) + (2/3) = 7/6
+    Fraction f4 = f1 - f2; // (1/2) - (2/3) = -1/6
+    Fraction f5 = f1 * f2; // (1/2) * (2/3) = 2/6 = 1/3
+    Fraction f6 = f1 / f2; // (1/2) / (2/3) = 3/4
+
+    Console.WriteLine("First fraction: " + f1);
+    Console.WriteLine("Second fraction: " + f2 + "\n");
+    Console.WriteLine("+: " + f3);
+    Console.WriteLine("-: " + f4);
+    Console.WriteLine("*: " + f5);
+    Console.WriteLine("/: " + f6);
+
+    public class Fraction
+    {
+        // Числитель
+        public int Numerator { get; set; }
+
+        // Знаменатель
+        public int Denominator { get; set; }
+
+        public Fraction(int numerator, int denominator)
+        {
+            Numerator = numerator;
+            Denominator = denominator;
+        }
+
+        public static Fraction operator +(Fraction f1, Fraction f2)
+        {
+            int numerator = (f1.Numerator * f2.Denominator) + (f2.Numerator * f1.Denominator);
+            int denominator = f1.Denominator * f2.Denominator;
+            return new(numerator, denominator);
+        }
+
+        public static Fraction operator -(Fraction f1, Fraction f2)
+        {
+            int numerator = (f1.Numerator * f2.Denominator) - (f2.Numerator * f1.Denominator);
+            int denominator = f1.Denominator * f2.Denominator;
+            return new(numerator, denominator);
+        }
+
+        public static Fraction operator *(Fraction f1, Fraction f2)
+        {
+            int numerator = f1.Numerator * f2.Numerator;
+            int denominator = f1.Denominator * f2.Denominator;
+            return new(numerator, denominator);
+        }
+
+        public static Fraction operator /(Fraction f1, Fraction f2)
+        {
+            int numerator = f1.Numerator * f2.Denominator;
+            int denominator = f1.Denominator * f2.Numerator;
+            return new(numerator, denominator);
+        }
+
+        public override string ToString()
+        {
+            return $"{Numerator}/{Denominator}";
+        }
+    }
+#endregion
