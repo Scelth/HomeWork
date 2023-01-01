e2613ec0fcc56040a276a17dd669c0b4fb6216e6
Scelth <120787169+Scelth@users.noreply.github.com>
2023-01-01 19:14:22 +0400

    Update C# homework №3.cs

--- C# №3/C# homework №3.cs
+++ C# №3/C# homework №3.cs
@@ -2,10 +2,10 @@
     Fraction f1 = new(1, 2); // 1/2
     Fraction f2 = new(2, 3); // 2/3
 
-    Fraction f3 = f1 + f2; // (1/2) + (2/3) = 7/6
-    Fraction f4 = f1 - f2; // (1/2) - (2/3) = -1/6
-    Fraction f5 = f1 * f2; // (1/2) * (2/3) = 2/6 = 1/3
-    Fraction f6 = f1 / f2; // (1/2) / (2/3) = 3/4
+    Fraction f3 = f1 + f2;
+    Fraction f4 = f1 - f2;
+    Fraction f5 = f1 * f2;
+    Fraction f6 = f1 / f2;
 
     Console.WriteLine("First fraction: " + f1);
     Console.WriteLine("Second fraction: " + f2 + "\n");
