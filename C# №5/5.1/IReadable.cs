80a7433fd9abdc5b1892ed877764814cbe6a1ab8
Scelth <120787169+Scelth@users.noreply.github.com>
2023-01-14 13:57:30 +0400

    Add files via upload

---
+++ C# №5/5.1/IReadable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRead
+{
+    interface IReadable
+    {
+        public string? Name { get; set; }
+        public string? Publishing_house { get; set; }
+        public short Year { get; set; }
+        public short Pages { get; set; }
+    }
+}
