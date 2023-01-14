540dc2e9689eabfc2cfb32418bba192ecd390de7
Scelth <120787169+Scelth@users.noreply.github.com>
2023-01-14 13:52:29 +0400

    Add files via upload

---
+++ C# №5/5.1/IMagazineable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMag
+{
+    interface IMagazineable
+    {
+        public string? Name { get; set; }
+        public string? Publishing_house { get; set; }
+        public short Year { get; set; }
+        public short Pages { get; set; }
+    }
+}
---
+++ C# №5/5.1/Magazine.cs
@@ -0,0 +1,26 @@
+﻿using IMag;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mag
+{
+    public class Magazine : IMagazineable
+    {
+        public string? Name { get; set; }
+        public string? Publishing_house { get; set; }
+        public short Year { get; set; }
+        public short Pages { get; set; }
+
+        public override string ToString()
+        {
+            return
+                $"\nMagazine - {Name}\n" +
+                $"Publishing house - {Publishing_house}\n" +
+                $"Release date - {Year}\n" +
+                $"Number of pages - {Pages}\n";
+        }
+    }
+}
