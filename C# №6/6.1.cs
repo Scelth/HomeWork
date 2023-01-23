c36c11cb87c7b683c1609d0f3ff71b6596a8fbf5
Scelth <120787169+Scelth@users.noreply.github.com>
2023-01-23 15:57:49 +0400

    Add files via upload

---
+++ C# №6/6.1.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("\n\tEnter your value!");
+Console.Write("\tA - ");
+int a = Int32.Parse(Console.ReadLine());
+Console.Write("\tB - ");
+int b = Int32.Parse(Console.ReadLine());
+MyClass.Swap(a, b);
+
+class MyClass
+{
+    public static void Swap<T>(T a, T b)
+    {
+        T temp = a;
+        a = b;
+        b = temp;
+
+        Console.WriteLine($"\n\tMethod Swap" +
+            $"\n\tA - {a}" +
+            $"\n\tB - {b}");
+    }
+}
