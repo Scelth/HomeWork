e7ea803ec0ce2c6c6d5809547a10633eb9c3d899
Scelth <120787169+Scelth@users.noreply.github.com>
2023-02-01 19:34:36 +0400

    Add files via upload

---
+++ C# №6/6.2.cs
@@ -0,0 +1,39 @@
+﻿public class PriQueue<T> where T : IComparable<T>
+{
+    private List<T> data;
+
+    public PriQueue()
+    {
+        this.data = new List<T>();
+    }
+
+    public void Enqueue(T item)
+    {
+        data.Add(item);
+        int a = data.Count - 1;
+        while (a > 0)
+        {
+            int b = (a - 1) / 2;
+            if (data[a].CompareTo(data[b]) >= 0)
+            {
+                break;
+            }
+
+            T tmp = data[a];
+            data[a] = data[b];
+            data[b] = tmp;
+            a = b;
+        }
+    }
+
+    public T Peek()
+    {
+        T item = data[0];
+        return item;
+    }
+
+    public int Count()
+    {
+        return data.Count;
+    }
+}
