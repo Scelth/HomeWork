2e2c81af479c9e4a20b1923e43f8e8eae88458f3
Scelth <120787169+Scelth@users.noreply.github.com>
2023-02-15 17:14:45 +0400

    Add files via upload

---
+++ WinForm № 2- 3/Presenter/SerializePresenter/DeserializePresenter.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ.Presenter.SerializePresenter
+{
+    public class DeserializePresenter : IDeserializeable
+    {
+        public DeserializePresenter()
+        {
+            using FileStream fs = new("People.json", FileMode.OpenOrCreate);
+            using StreamReader sr = new(fs);
+
+            Deserialize(sr.ReadToEnd());
+            var res = JsonConvert.DeserializeObject("People.json");
+        }
+
+        public void Deserialize(string json)
+        {
+            //var res = JsonConvert.DeserializeObject(json).ToString;
+            //return (string)res;
+        }
+    }
+}
