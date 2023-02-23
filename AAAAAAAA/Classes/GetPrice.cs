82e33bfc11970b297e10898ace002ff54ba40657
Scelth <120787169+Scelth@users.noreply.github.com>
2023-02-23 21:56:05 +0400

    Add files via upload

---
+++ AAAAAAAA/Classes/GetPrice.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Cash_Register.Classes
+{
+    internal class GetPrice
+    {
+        public double price;
+
+        public string SetPrice(ComboBox gasComboBox, ComboBoxItem A_92, ComboBoxItem A_95, ComboBoxItem A_98)
+        {
+            if (gasComboBox.SelectedItem == A_92)
+            {
+                price = 0.471;
+            }
+
+            else if (gasComboBox.SelectedItem == A_95)
+            {
+                price = 0.785;
+            }
+
+            else if (gasComboBox.SelectedItem == A_98)
+            {
+                price = 0.838;
+            }
+
+            return price.ToString();
+        }
+    }
+}
---
+++ AAAAAAAA/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Cash_Register.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Cash_Register
+{
+    public partial class MainWindow : Window
+    {
+        private IPriceable gasPrice = new GasPrice();
+        private LiterOrCash literOrCash = new();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Sum_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void gasComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            gasPrice.Price(gasComboBox, A_92, A_95, A_98, priceTextBlock);
+        }
+
+        private void gasButton_Click(object sender, RoutedEventArgs e)
+        {
+            literOrCash.Pay((RadioButton)sender, litersTextBox, cashTextBox, paymentTextBlock);
+        }
+    }
+}
