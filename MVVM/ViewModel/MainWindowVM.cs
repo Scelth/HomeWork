4f60c699c66594ecca0ac8e22664dc7e4952ba67
Scelth <120787169+Scelth@users.noreply.github.com>
2023-03-04 21:33:06 +0400

    MVVM added

---
+++ MVVM/ViewModel/MainWindowVM.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Controls;
+using System.Windows.Input;
+using MVVM.View.UserControls;
+
+namespace MVVM.ViewModel
+{
+    public class MainWindowVM : ViewModelBase
+    {
+        private object selectedPage;
+
+        public object SelectedPage
+        {
+            get { return selectedPage; }
+            set
+            {
+                if (selectedPage != value)
+                {
+                    selectedPage = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+        public void FirstButtonClicked()
+        {
+            SelectedPage = new FirstPage();
+        }
+
+        public void SecondButtonClicked()
+        {
+            SelectedPage = new SecondPage();
+        }
+
+        public void ThirdButtonClicked()
+        {
+            SelectedPage = new ThirdPage();
+        }
+    }
+}
