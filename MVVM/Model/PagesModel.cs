d50b294e446b68b897079b12adce7d6ceb845d68
Scelth <120787169+Scelth@users.noreply.github.com>
2023-03-05 21:24:47 +0400

    MVVM replaced

---
+++ MVVM/Model/PagesModel.cs
@@ -0,0 +1,28 @@
+﻿using MVVM.View.UserControls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM.Model
+{
+    internal class PagesModel : IPagesModel
+    {
+        public FirstPage FirstButtonClicked()
+        {
+            return new FirstPage();
+        }
+
+        public SecondPage SecondButtonClicked()
+        {
+            return new SecondPage();
+        }
+
+        public ThirdPage ThirdButtonClicked()
+        {
+            return new ThirdPage();
+        }
+    }
+
+}
