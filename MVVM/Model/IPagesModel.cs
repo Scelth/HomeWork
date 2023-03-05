59669420e677efc87582370e2ad5b8676ca9d15f
Scelth <120787169+Scelth@users.noreply.github.com>
2023-03-05 21:23:54 +0400

    Add files via upload

---
+++ MVVM/Model/IPagesModel.cs
@@ -0,0 +1,13 @@
+﻿using MVVM.View.UserControls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM.Model
+{
+    public interface IPagesModel
+    {
+    }
+}
---
+++ MVVM/ViewModel/MainWindowVM.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Controls;
+using System.Windows.Input;
+using MVVM.Model;
+using Prism.Commands;
+using Prism.Mvvm;
+
+
+namespace MVVM.ViewModel
+{
+    public class MainWindowVM : BindableBase
+    {
+        private PagesModel pagesModel { get; set; }
+        private object selectedPage;
+        public object SelectedPage
+        {
+            get { return selectedPage; }
+            set
+            {
+                if (selectedPage != value)
+                {
+                    selectedPage = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+        public DelegateCommand FirstButtonCommand { get; }
+        public DelegateCommand SecondButtonCommand { get; }
+        public DelegateCommand ThirdButtonCommand { get; }
+
+        public MainWindowVM()
+        {
+            pagesModel = new PagesModel();
+
+            FirstButtonCommand = new DelegateCommand(FirstButtonClicked);
+            SecondButtonCommand = new DelegateCommand(SecondButtonClicked);
+            ThirdButtonCommand = new DelegateCommand(ThirdButtonClicked);
+        }
+
+        private void FirstButtonClicked()
+        {
+            SelectedPage = pagesModel.FirstButtonClicked();
+        }
+
+        private void SecondButtonClicked()
+        {
+            SelectedPage = pagesModel.SecondButtonClicked();
+        }
+
+        private void ThirdButtonClicked()
+        {
+            SelectedPage = pagesModel.ThirdButtonClicked();
+        }
+    }
+}
