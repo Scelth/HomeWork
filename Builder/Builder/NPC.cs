a9364ad13be294f01ab7a9644e220ec411aacdef
Scelth <120787169+Scelth@users.noreply.github.com>
2023-04-06 13:40:06 +0400

    Delete Builder directory

--- Builder/Builder/NPC.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Builder;
-
-internal class NPC
-{
-    public string Name { get; set; }
-    public int Health { get; set; }
-    public int Stamina { get; set; }
-    public int Difficulty { get; set; }
-}
