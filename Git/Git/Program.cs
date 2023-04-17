9025a80b7d0050e66895d4378eaf14c5238404f5
Scelth <magomed.guluyev@gmail.com>
2023-04-17 16:39:53 +0400

    Git added

---
+++ Git/Git/Program.cs
@@ -0,0 +1,8 @@
+﻿using Git.Model;
+using Git.Realization;
+
+NPCModel model = new();
+model.Name = "Miraak";
+
+NPCRealization NPC = new(model.Name);
+NPC.Greet();
---
+++ Git/Git/Realization/NPCRealization.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git.Realization
+{
+    internal class NPCRealization
+    {
+        public string Name { get; set; }
+
+        public NPCRealization(string _name)
+        {
+            Name = _name;
+        }
+
+        public void Greet()
+        {
+            Console.WriteLine($"\n\t{Name} says: Welcome, hero!!");
+        }
+
+        public void Talk()
+        {
+            Console.WriteLine($"\t{Name} says: I'll tell you about our village");
+        }
+
+        public void Farewell()
+        {
+            Console.WriteLine($"\t{Name} says: Bye, see you later!");
+        }
+    }
+}
