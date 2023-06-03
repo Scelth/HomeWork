777bcfd09f29183cc8b5c3344be2beacc519de0c
Scelth <magomed.guluyev@gmail.com>
2023-06-03 13:54:28 +0400

    Wiki added

---
+++ Wiki/README.md
@@ -0,0 +1,10 @@
+Create an application on the WPF of the Country. You need to store in the database: 
+    ■ Country name
+    ■ Year of creation
+    ■ Type of board
+    ■ Link to the image of the country map 
+    ■ Population 
+    ■ Area
+    ■ GDP (Gross Domestic Product)
+
+    ■ Withdrawal of all countries
---
+++ Wiki/Wiki/Model/Country.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wiki.Model
+{
+    public class Country
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime Year { get; set; }
+
+        public string TypeOfBoard { get; set; }
+
+        public string Map { get; set; }
+
+        public int Population { get; set; }
+
+        public int Area { get; set; }
+
+        public int GDP { get; set; }
+    }
+}
