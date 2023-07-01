eb7fb0520ee456abfc92ff0513d21a1eff1876c1
Scelth <magomed.guluyev@gmail.com>
2023-07-01 17:23:08 +0400

    Download added

--- Download/Download/Program.cs
+++ Download/Download/Program.cs
@@ -1,4 +1,4 @@
-namespace HW1
+namespace Download
 {
     internal static class Program
     {
@@ -11,7 +11,7 @@ namespace HW1
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new Torrent());
         }
     }
 }
